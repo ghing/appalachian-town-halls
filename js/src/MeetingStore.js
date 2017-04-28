7dce2b69c17b696ad7901d7ac03fc44f8847d838
Geoff Hing <geoffhing@gmail.com>
2017-04-27 23:17:30 -0500

    Implement store for meetings
    Implement a class that transforms the raw JSON of officials and
    meetings and provides some useful getter/setter methods for accessing
    the data.

--- js/src/MeetingStore.js
+++ js/src/MeetingStore.js
@@ -1,6 +1,66 @@
 class MeetingStore {
-  constructor(meetings) {
-    this._meetings = meetings;
+  constructor() {
+    this._meetings = [];
+    this._meetingsByDate = {};
+    this._officials = [];
+  }
+
+  setOfficials(officials) {
+    this._officials = officials;
+    this._setMeetingsFromOfficials(this._officials);
+  }
+
+  getOfficials() {
+    return this._officials;
+  }
+
+  _setMeetingsFromOfficials(officials, sort = true) {
+    this._meetings = [];
+    this._meetingsByDate = {};
+
+    officials.forEach(official => {
+      official.meetings.forEach(meeting => this.addMeeting(meeting));
+    });
+
+    if (sort) {
+      this._sortMeetingsByDate();
+    }
+
+    return this;
+  }
+
+  _sortMeetingsByDate() {
+    this._meetings.sort((a, b) => {
+      if (a.date > b.date) {
+        return -1;
+      }
+
+      if (a.date < b.date) {
+        return 1;
+      }
+
+      return 0;
+    });
+  }
+
+  addMeeting(meeting) {
+    this._meetings.push(meeting);
+
+    if (!this._meetingsByDate[meeting.date]) {
+      this._meetingsByDate[meeting.date] = [];
+    }
+
+    this._meetingsByDate[meeting.date].push(meeting);
+
+    return this;
+  }
+
+  getMeetingsForDate(date) {
+    return this._meetingsByDate[date];
+  }
+
+  getMeetings() {
+    return this._meetings;
   }
 }
 
--- js/src/app.js
+++ js/src/app.js
@@ -1,6 +1,16 @@
+var d3 = require('d3');
+
+import MeetingStore from './MeetingStore';
+
 export class App {
   constructor(options) {
     this._timelineContainer = options.timelineContainer;
-  }
+    this._meetingStore = new MeetingStore();
 
+    d3.json(options.officialMeetingsJsonUrl, data => {
+      this._meetingStore.setOfficials(data.objects);
+
+      console.log(this._meetingStore.getMeetings());
+    });
+  }
 }
